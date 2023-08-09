89e38f56e819451140f0f389bc31d300f26e1cbc
Colin Jaffe <balloonasaurus@gmail.com>
2023-08-08 22:29:10 -0400

    Redoes directory structure.
    - Moves main script to index.js.
    - Moves all example usage code to example assignment repository.

--- interface.js
+++
@@ -1,7 +0,0 @@
-const userInput = prompt(
-  "Enter a temperature in degrees Celsius or Fahrenheit"
-);
-
-const cleanInput = removeSpaces(userInput);
-const answer = convertTemp(cleanInput);
-console.log(`${cleanInput} converted is:\n\n${answer} degrees.`);
